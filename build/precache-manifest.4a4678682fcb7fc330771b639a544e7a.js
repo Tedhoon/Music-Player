9b4485f35019cb12a8cc2133407e6db55695d526
tedhoon <gt0305@likelion.org>
2020-09-08 17:57:13 +0900

    [mdf] fix of drag & re-build

---
+++ build/precache-manifest.4a4678682fcb7fc330771b639a544e7a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2740d5e1b84180c657cb426901c324",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef108e44492cbd4a3adc",
+    "url": "/static/css/2.baae8b2c.chunk.css"
+  },
+  {
+    "revision": "ff8f3c295e86eea25caf",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef108e44492cbd4a3adc",
+    "url": "/static/js/2.0f721197.chunk.js"
+  },
+  {
+    "revision": "7370a55f5a19d2f8a4c56993c046de21",
+    "url": "/static/js/2.0f721197.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff8f3c295e86eea25caf",
+    "url": "/static/js/main.2658fb65.chunk.js"
+  },
+  {
+    "revision": "290683185e573683b1f3",
+    "url": "/static/js/runtime-main.f3d07ac8.js"
+  },
+  {
+    "revision": "30693a95d56ede43a1564083350d5de5",
+    "url": "/static/media/1.30693a95.mp3"
+  },
+  {
+    "revision": "75419257e302bb52f6a2047e7753eed5",
+    "url": "/static/media/1.75419257.jpg"
+  },
+  {
+    "revision": "5d0f0eed6b4478bde292b2df296cb1d5",
+    "url": "/static/media/2.5d0f0eed.jpg"
+  },
+  {
+    "revision": "5d0f0eed6b4478bde292b2df296cb1d5",
+    "url": "/static/media/3.5d0f0eed.jpg"
+  }
+]);
--- src/Pages/MainMusicPage.js
+++ src/Pages/MainMusicPage.js
@@ -11,54 +11,34 @@ function MainMusicPage () {
         console.log(id)
         switch (id) {
             case "MUSICTHEME1":
-                console.log("1실행")
-                // setPlayList([...playList, MUSICTHEME1[0],MUSICTHEME1[1],MUSICTHEME1[2]])
-                // setPlayList([...playList, MUSICTHEME1])
-
                 setPlayList([...playList, ...MUSICTHEME1])
-                // console.log([...playList])
-                // console.log(typeof(MUSICTHEME1))
-                // console.log(typeof(MUSICTHEME1[0],MUSICTHEME1[2]))
-                // console.log(MUSICTHEME1)
-                // console.log([...MUSICTHEME1])
-                // console.log(MUSICTHEME1[0],MUSICTHEME1[1],MUSICTHEME1[2])
-                // console.log([MUSICTHEME1[0], MUSICTHEME1[1], MUSICTHEME1[2]])
                 break;
+
             case "MUSICTHEME2":
-                console.log("2실행")
                 setPlayList([...playList, ...MUSICTHEME2])
                 break;
+
             default:
                 return;
-
         }
     }
 
     const addMusic = (e) => {
         setPlayList([...playList, MUSICTHEME1[e.target.id]])
     }
-
-    useEffect(() => {
-        // console.log(playList);
-        // console.log(...playList)
-        // console.log([...playList, [MUSICTHEME2[0]]])
-        
-        // console.log([...playList] + MUSICTHEME2[0])
-        // console.log(MUSICTHEME1)
-        // console.log(MUSICTHEME2)
-        // console.log(MUSICTHEME2[0])
-        // console.log([playList])
-    }, [playList])
-
+    
+    // console.log(playList)
     return (
         <>
-
             {
                 MUSICTHEME1.map((music, key) => <button onClick={addMusic} id={key}>{music.name}</button>)
             }
             <button onClick={playMusicTheme} id="MUSICTHEME1">테마 1</button>
             <button onClick={playMusicTheme} id="MUSICTHEME2">테마 2</button>
-            {playList && <MusicPlayer playList={playList} />}        
+            <MusicPlayer 
+                playList={playList} 
+            />        
+
         </>
     )
 }
